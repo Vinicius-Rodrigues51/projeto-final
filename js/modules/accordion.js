f2a11464b33782de1c830e402d1c91db1a7071e1
Vinicius Rodrigues <vinicius.motta.09@hotmail.com>
2021-12-14 23:24:10 -0300

    mudança nos accordions

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,16 +1,28 @@
-export default function initTabFaq() {
-    const accordionList = document.querySelectorAll('.js-accordion dt')
-    if(accordionList.length) {
-        accordionList[0].classList.add('ativo')
-        accordionList[0].nextElementSibling.classList.add('ativo')
+export default class Accordions {
+    constructor(list, className) {
+        this.accordionList = document.querySelectorAll(list);
+        this.activeClass = className || 'ativo' // da a opção do usuario escolher o nome da classe usada, se n for declarada, por padrao usa 'ativo'
+    }
 
-        function activeAccordion() {
-            this.classList.toggle('ativo')
-            this.nextElementSibling.classList.toggle('ativo');
-        }
+    // cria a funcionalidade de dar toggle aos itens 
+    toggleAccordion(item) {
+        item.classList.toggle(this.activeClass);
+        item.nextElementSibling.classList.toggle(this.activeClass);
+    }
 
-        accordionList.forEach(item => {
-            item.addEventListener('click', activeAccordion);
+    // adiciona a funcionalidade de toggle para todos os itens da lista
+    addAccordionEvent() {
+        this.accordionList.forEach(item => {
+            item.addEventListener('click', () => this.toggleAccordion(item));
         })
+    }
+
+    // inicia a funcionalidade da classe
+    init() {
+        if (this.accordionList.length) { // verifica se existe item na lista para iniciar o código
+            this.toggleAccordion(this.accordionList[0]) // ativa o primeiro item da lista para quando site for aberto
+            this.addAccordionEvent();
+        }
+        return this;
     }
 }
